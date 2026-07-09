d84e7bff415cf41dd805b30adf7811ddb71279b1
catalinacarrasco-beep <catalina.carrasco@grantt.cl>
2026-07-09 10:34:11 -0400

    SW: wait for SKIP_WAITING message before activating

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,8 @@
-self.addEventListener('install', () => self.skipWaiting())
+self.addEventListener('install', () => {})
 self.addEventListener('activate', e => e.waitUntil(clients.claim()))
+self.addEventListener('message', e => {
+  if (e.data === 'SKIP_WAITING') self.skipWaiting()
+})
 self.addEventListener('fetch', e => {
   if (e.request.method !== 'GET') return
   e.respondWith(fetch(e.request).catch(() => caches.match(e.request)))
